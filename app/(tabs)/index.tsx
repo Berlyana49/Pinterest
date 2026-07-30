3252a028cecf3f5632b25cea0dceba43ec43915b
Berlyana49 <bavriliyanti@gmail.com>
2026-07-30 08:30:15 +0700

    Home screen: masonry layout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,43 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, FlatList, Image, ScrollView } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { View } from '@/components/Themed';
+import Pin from '@/components/Pin';
+import pins from '@/assets/data/pins';
 
-export default function TabOneScreen() {
+export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        
+        <View style={styles.column}>
+
+          {pins
+            .filter((item, index) => index % 2 === 0)
+            .map((pin) => (
+              <Pin pin={pin} key={pin.id} />
+            ))}
+        </View>
+
+        <View style={styles.column}>
+          
+          {pins
+            .filter((item, index) => index % 2 === 1)
+            .map((pin) => (
+              <Pin pin={pin} key={pin.id} />
+            ))}
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
+    flexDirection: 'row',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  column: {
+    flex:1,
   },
 });
