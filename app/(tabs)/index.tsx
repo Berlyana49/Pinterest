fe23887970b4d2127f7c17a711e6454d708c5afe
Berlyana49 <bavriliyanti@gmail.com>
2026-08-06 08:25:40 +0700

    pin

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,43 @@
+import { StyleSheet, FlatList, Image, ScrollView } from 'react-native';
+
+import EditScreenInfo from '@/components/EditScreenInfo';
+import { View } from '@/components/Themed';
+import Pin from '@/components/Pin';
+import pins from '@/assets/data/pins';
+
+export default function HomeScreen() {
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        
+        <View style={styles.column}>
+
+          {pins
+            .filter((item, index) => index % 2 === 0)
+            .map((pin) => (
+              <Pin pin={pin} key={pin.id} />
+            ))}
+        </View>
+
+        <View style={styles.column}>
+          
+          {pins
+            .filter((item, index) => index % 2 === 1)
+            .map((pin) => (
+              <Pin pin={pin} key={pin.id} />
+            ))}
+        </View>
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    flexDirection: 'row',
+  },
+  column: {
+    flex:1,
+  },
+});
---
+++ components/Pin.tsx
@@ -0,0 +1,75 @@
+import {View, Image, Text, StyleSheet, Pressable} from "react-native";
+import { Entypo } from '@expo/vector-icons';
+import {useState, useEffect} from "react";
+
+
+const Pin = (props) => {
+    const { image, title } = props.pin;
+
+    const [ratio, setRatio] = useState(1);
+
+
+    useEffect(() => {
+        if (image) {
+            Image.getSize(
+                image, 
+                (width, height) => setRatio(width / height),
+                (error) => console.log(error)
+            );
+        }
+    }, [image]);
+
+    const onLike = () => {};
+
+    
+    return (
+        <View style={styles.pin}>
+            <View> 
+                <Image 
+                    source={{ 
+                    uri: image, 
+                    }} 
+                    style={[styles.image, {aspectRatio: ratio}]}
+                />
+
+                <Pressable onPress={onLike} style ={styles.heartBtn}>
+                    <Entypo name="heart-outlined" size={16} color="black" />
+                </Pressable>
+            </View>
+            <Text style={styles.title} numberOfLines={2}>
+                {title}
+            </Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+  pin: {
+    width: "100%",
+    padding: 4,
+  },
+
+  title: {
+    fontSize: 16,
+    lineHeight: 22,
+    fontWeight: "600",
+    margin: 5,
+    color: "#181818",
+  },
+
+  image: {
+    width: "100%",
+    borderRadius: 15,
+  },
+
+  heartBtn: {
+    backgroundColor: '#D3CFD4',
+    position: 'absolute',
+    bottom: 10,
+    right: 10,
+    padding: 5,
+    borderRadius: 50,
+  },
+});
+
+export default Pin;
